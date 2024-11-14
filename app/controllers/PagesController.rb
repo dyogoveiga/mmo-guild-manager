486d60de6e11488d39dbe8a47208c836f8df4b27
Victor Alencastro <vicfelipe@live.com>
2024-11-14 14:46:23 -0300

    Initial template

---
+++ app/controllers/PagesController.rb
@@ -0,0 +1,7 @@
+class PagesController < ApplicationController
+    layout 'pages'
+  
+    def about; end
+    def contact; end
+    def store; end
+  end
--- config/routes.rb
+++ config/routes.rb
@@ -10,5 +10,5 @@ Rails.application.routes.draw do
   get "manifest" => "rails/pwa#manifest", as: :pwa_manifest
 
   # Defines the root path route ("/")
-  root to: "homepages#show"
+  root to: "dashboards#index"
 end
